291332c94cf41c519874a98d9128d57a0d91f362
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-09-02 18:03:11 +0300

    ActivityIndicator

---
+++ ActivityIndicator/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
+import { ActivityIndicator, StyleSheet, Text,  View } from 'react-native';
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() =>{
+      setIsLoading(false);
+    },3000);
+  }, [])
+  
+  
+  return (
+    <View style={styles.container}>
+      {isLoading &&
+      <ActivityIndicator size="large" animating={true} />
+      }
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
