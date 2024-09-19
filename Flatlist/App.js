71509e273acfc44a6844cd1a7451e9c4e122c9bf
Juha-Matti Huhta <118987393+AvaRaGane@users.noreply.github.com>
2024-09-19 08:08:14 +0300

    Tallennus toimii..

--- Flatlist/App.js
+++ Flatlist/App.js
@@ -5,6 +5,9 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import Add from './components/Add';
 import uuid from 'react-native-uuid';
 import Search from './components/Search';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STORAGE_KEY = 'teppo'
 
 export default function App() {
   const [data, setData] = useState([]) //add versiossa 
@@ -13,6 +16,37 @@ export default function App() {
   const items = useMemo(() =>
   criteria.length > 0 ? data.filter((item) =>item.name.startsWith(criteria)) : data,[data,criteria])
   
+  useEffect(() => {
+    storeData(data)
+  }, [data])
+
+  useEffect(() => {
+    //AsyncStorage.clear()
+    getData()
+  }, [])
+
+  const getData = async() => {
+    try {
+      const value = await AsyncStorage.getItem(STORAGE_KEY)
+      let json = JSON.parse(value)
+      if (json === null) {
+        json = []
+      }
+      setData(json)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  const storeData = async(value) => {
+    try {
+      const json = JSON.stringify(value)
+      await AsyncStorage.setItem(STORAGE_KEY,json)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
   const add = useCallback((name) => {
     const newItem = {
       id: uuid.v4(),
